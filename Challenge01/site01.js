92c4478aab964f208df9707ac86e0ca1add83030
John Flynn <johnbishopflynn@gmail.com>
2023-06-25 05:01:55 -0400

    Added js and pseudocode.

---
+++ Challenge01/site01.js
@@ -0,0 +1,35 @@
+const marvelHeros = [
+    "Ant-Man",
+    "Black Panther",
+    "Black Widow",
+    "Captian America",
+    "Doctor Strange",
+    "Hawkeye",
+    "Hulk",
+    "Iron Man",
+    "Luke Cage",
+    "Moon Knight",
+    "Ms. Marvel",
+    "Scarlet Witch",
+    "Spider-Man",
+    "Thor",
+    "Wasp"
+]
+//driver function used for display and passing values.
+function findHero()
+{
+    //implement funtion findLongestString that returns the longest word.
+    let lowrd findLongestString(marvelHeros);
+
+    //used for display. no need to change
+    document.getElementById("result").innerHTML = lword;
+
+    //extra creidt display all of the heroes to the page
+
+}
+
+//takes an array of string and returns the longest one.
+function findLongestString(namsArray)
+{
+    return "";
+}
